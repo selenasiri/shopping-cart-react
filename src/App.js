ecd0c1da465450e306338e5365aca8281182a3b5
Selena Siri <selenasiri@gmail.com>
2020-11-04 20:04:30 -0500

    photos and changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import { ReactComponent } from '*.svg'
 import React from 'react'
+import data from "./data.json"
 
-function App() {
+export App extends ReactComponent {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    }
+  }
+
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+        <div className="content">
+          <div className="main">
+            Products
+          </div>
+          <div className="sidebar">
+            Cart Items
+          </div>
+        </div>
       </main>
       <footer>All rights reserved.</footer>
     </div>
---
+++ src/components/Products.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="products">
+          {this.props.products.map(product => (
+            <li key={product._id}>
+              <div className="product">
+                <a href="#">
+                  <img src={product.image} alt="product image"/></img>
+                  <p>
+                    {product.title}
+                  </p>
+                </a>
+                <div className="product-price">
+                  <div>{formatCurrency(product.price)}</div>
+                  <button onClick={() => this.props.addToCart(product)}
+                  className="button primary">
+                    Add To Cart
+                  </button>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
